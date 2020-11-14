d5029b552c3c798249f6719662a6111d171b6d86
NanaSlav <slavina270@gmail.com>
2020-11-14 18:22:29 +0300

    Authentication with the user from db

--- planner/src/main/java/ru/nanaslav/planner/model/Account.java
+++ planner/src/main/java/ru/nanaslav/planner/model/Account.java
@@ -1,12 +1,16 @@
 package ru.nanaslav.planner.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Collection;
 
 @Entity
-public class Account {
+public class Account implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -42,5 +46,30 @@ public class Account {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
--- planner/src/main/java/ru/nanaslav/planner/repository/AccountRepository.java
+++ planner/src/main/java/ru/nanaslav/planner/repository/AccountRepository.java
@@ -3,4 +3,6 @@ package ru.nanaslav.planner.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nanaslav.planner.model.Account;
 
-public interface AccountRepository  extends JpaRepository<Account, Long> {}
+public interface AccountRepository  extends JpaRepository<Account, Long> {
+    Account findByUsername(String username);
+}
