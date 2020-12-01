72e533a82df9b1f4f904749fa3abb0453ac4a688
NanaSlav <slavina270@gmail.com>
2020-12-01 18:49:28 +0300

    task done status is added

--- planner/src/main/java/ru/nanaslav/planner/controller/TaskController.java
+++ planner/src/main/java/ru/nanaslav/planner/controller/TaskController.java
@@ -2,10 +2,7 @@ package ru.nanaslav.planner.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.nanaslav.planner.model.Project;
 import ru.nanaslav.planner.model.Task;
 import ru.nanaslav.planner.repository.ProjectRepository;
@@ -27,6 +24,7 @@ public class TaskController {
 
         Project project = projectRepository.findById(project_id).orElseThrow(IllegalStateException::new);
         Task task = new Task(title, description, project);
+        task.setDone(false);
         taskRepository.save(task);
         // TODO: add view - tasks
         return "tasks";
@@ -36,6 +34,13 @@ public class TaskController {
     public String showAddTask() {
         // TODO: add view - add task
         return "add_task";
+    }
+
+    @PostMapping("/{id}/done")
+    public String setDone(@PathVariable long id) {
+        Task task = taskRepository.findById(id).orElseThrow(IllegalStateException::new);
+        task.setDone(true);
+        return "";
     }
 
 }
--- planner/src/main/java/ru/nanaslav/planner/model/Task.java
+++ planner/src/main/java/ru/nanaslav/planner/model/Task.java
@@ -13,6 +13,8 @@ public class Task {
 
     private String name;
 
+    private boolean isDone;
+
     @Column(columnDefinition = "TEXT")
     private String description;
 
