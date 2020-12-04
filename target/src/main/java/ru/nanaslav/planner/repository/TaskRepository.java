7dfd27d766116ab35912bdf11d06ca67ce90d900
NanaSlav <slavina270@gmail.com>
2020-12-04 18:31:57 +0300

    extra files is deleted

--- target/src/main/java/ru/nanaslav/planner/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.nanaslav.planner.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.nanaslav.planner.model.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
