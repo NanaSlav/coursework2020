6b622b4a03c172c903248816a23ca2813108e485
NanaSlav <slavina270@gmail.com>
2020-12-14 00:17:12 +0300

    projects list

--- src/main/java/ru/nanaslav/planner/controller/ProjectController.java
+++ src/main/java/ru/nanaslav/planner/controller/ProjectController.java
@@ -2,14 +2,18 @@ package ru.nanaslav.planner.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.nanaslav.planner.model.Account;
+import ru.nanaslav.planner.model.Participant;
 import ru.nanaslav.planner.model.Project;
 import ru.nanaslav.planner.repository.AccountRepository;
 import ru.nanaslav.planner.repository.ProjectRepository;
 import ru.nanaslav.planner.service.ProjectService;
+
+import java.util.List;
 
 /**
  * Controller for project
@@ -25,6 +29,13 @@ public class ProjectController {
     AccountRepository accountRepository;
     @Autowired
     ProjectService projectService;
+
+    @GetMapping("/")
+    public String showProjectsList(@AuthenticationPrincipal Account account, Model model) {
+        List<Project> projects = projectService.getProjectsByAccount(account);
+        model.addAttribute("projects", projects);
+        return "project/projects-list";
+    }
 
     @GetMapping("/{projectId}")
     public String showProjectPage(@PathVariable long projectId,
